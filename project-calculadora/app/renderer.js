346533851b09f0dc5eb2874883e1757fa1703da3
NATHAN OLIVEIRA DOS SANTOS <a92293883@rn.senac.local>
2025-10-30 20:49:16 -0300

    :books: atualização

--- project-calculadora/app/renderer.js
+++ project-calculadora/app/renderer.js
@@ -3,12 +3,15 @@ const captacao = () => {
     
     teclado.addEventListener('click', (event) => {
         const botao = event.target.textContent;
+        let caixa = document.getElementById('caixa').value
         if(botao === 'CE'){document.getElementById('caixa').value = ''} 
         else if(botao.trim() === '='){
             const caixa = document.getElementById('caixa').value
             window.api.operar(caixa)
             document.getElementById('caixa').value = window.api.resolucao()
         }
+        else if(botao === 'DEL'){
+            document.getElementById('caixa').value = document.getElementById('caixa').value.slice(0, -1)}
         else{document.getElementById('caixa').value += botao}
     })
 }
